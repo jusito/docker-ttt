f22fe7a1ce47cb48ff4a0156242c2ab8b17126c3
Lucas Briese <lbriese@mail.uni-paderborn.de>
2018-09-19 21:16:51 +0200

    + initial files, + force workshop support, + auto mount support

---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "starting entrypoint.sh"
+set -e
+
+echo "installing / updating steamcmd"
+cd "$STEAM_PATH"
+wget -q -O - "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -zxvf -
+
+if [ -e "${STEAM_PATH}/server/steam_cache" ]; then
+	echo "clearing steam cache"
+	rm -rf "${STEAM_PATH}/server/steam_cache/"
+fi
+if [ -e "${STEAM_PATH}/server/garrysmod/cache" ]; then
+	echo "clearing cache"
+	rm -rf "${STEAM_PATH}/server/garrysmod/cache/*"
+fi
+
+echo "testing steamcmd"
+./steamcmd.sh +login anonymous +quit
+
+echo "installing / validating ttt"
+cd "$STEAM_PATH"
+./steamcmd.sh +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit || \
+	(echo '[error][1] catched => printing stderr.txt:' && \
+		cat "Steam/logs/stderr.txt" && \
+		echo '[error][1] <= printed' && \
+		./steamcmd.sh +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit) || \
+	(echo '[error][2] catched => printing stderr.txt:' && \
+		cat "Steam/logs/stderr.txt" && \
+		echo '[error][2] <= printed' && \
+		./steamcmd.sh +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit)
+
+bash installAndMountAddons.sh
+bash forceWorkshopDownload.sh
+
+echo "processing scripts before start"
+if [ -e "$SERVER_PATH/custom.sh" ]; then
+	echo "existing: $SERVER_PATH/custom.sh"
+	bash "$SERVER_PATH/custom.sh"
+else
+	echo "not existing: $SERVER_PATH/custom.sh"
+fi
+
+# todo catch => send killserver / quit
+cd "$STEAM_PATH/server/"
+trap 'pkill -15 srcds_run' SIGTERM
+./srcds_run -console -game garrysmod +gamemode terrortown "$@" &
+wait "$!"
---
+++ installAndMountAddons.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+mount='"mountcfg"'$'\n{\n'
+if [ "$INSTALL_CSS" = "true" ]; then
+	echo "installing & mounting css"
+	./steamcmd.sh +login anonymous +force_install_dir "$CSS_PATH" +app_update 232330 validate +quit
+	mount=${mount}'        "cstrike"    "'"${CSS_PATH}/cstrike"'"\n'
+	if [ "$INSTALL_HL2" != "true" ]; then
+		mount=${mount}'        "hl2"    "'"${CSS_PATH}/hl2"'"\n'
+	fi
+fi
+if [ "$INSTALL_HL2" = "true" ]; then
+	echo "installing & mounting hl2"
+	./steamcmd.sh +login anonymous +force_install_dir "$HL2_PATH" +app_update 232370 validate +quit
+	mount=${mount}'        "hl2"    "'"${HL2_PATH}/hl2"'"\n'
+	mount=${mount}'        "hl2mp"    "'"${HL2_PATH}/hl2mp"'"\n'
+fi
+if [ "$INSTALL_TF2" = "true" ]; then
+	echo "installing & mounting tf2"
+	./steamcmd.sh +login anonymous +force_install_dir "$TF2_PATH" +app_update 232250 validate +quit
+	mount=${mount}'        "tf2"    "'"${TF2_PATH}/tf"'"\n'
+	if [ "$INSTALL_CSS" != "true" ] && [ "$INSTALL_HL2" != "true" ]; then
+		mount=${mount}'        "hl2"    "'"${TF2_PATH}/hl2"'"\n'
+	fi
+fi
+if [ "$INSTALL_HLDM" = "true" ]; then
+	echo "installing & mounting hldm"
+	./steamcmd.sh +login anonymous +force_install_dir "$HLDM_PATH" +app_update 255470 validate +quit
+	mount=${mount}'        "hl1"    "'"${HLDM_PATH}/hl1"'"\n'
+	mount=${mount}'        "hldm"    "'"${HLDM_PATH}/hldm"'"\n'
+	if [ "$INSTALL_CSS" != "true" ] && [ "$INSTALL_HL2" != "true" ] && [ "$INSTALL_TF2" != "true" ]; then
+		mount=${mount}'        "hl2"    "'"${HLDM_PATH}/hl2"'"\n'
+	fi
+fi
+mount=${mount}$'}'
+
+
+if [ ! -e "${SERVER_PATH}/garrysmod/cfg" ]; then
+	mkdir -p "${SERVER_PATH}/garrysmod/cfg"
+fi
+if [ -e "${SERVER_PATH}/garrysmod/cfg" ]; then
+	rm "${SERVER_PATH}/garrysmod/cfg"
+fi
+touch "${SERVER_PATH}/garrysmod/cfg"
+echo "$mount" > "${SERVER_PATH}/garrysmod/cfg/mount.cfg"
+
