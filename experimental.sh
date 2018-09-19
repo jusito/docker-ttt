fa66695b598dc3d2100a71ea81a27ccb593144e6
Lucas Briese <lbriese@mail.uni-paderborn.de>
2018-09-19 23:10:57 +0200

    + default server.cfg used from this repo

--- experimental.sh
+++ experimental.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+#create default server.config
+#TODO don't miss to change to master!
+if [ ! -e "{$SERVER_PATH}/garrysmod/cfg/server.cfg" ]; then
+	mkdir -p "{$SERVER_PATH}/garrysmod/cfg"
+	wget -O "{$SERVER_PATH}/garrysmod/cfg/server.cfg" "https://raw.githubusercontent.com/jusito/docker-ttt/develop/server.cfg"
+fi
+
 #this is a simple option for myself, but you can use it too
 if [ "$USE_MY_REPLACER_CONFIG" = "true" ]; then
 	mkdir -p "{$SERVER_PATH}/garrysmod/data/jusito_ttt_entity_replace"
