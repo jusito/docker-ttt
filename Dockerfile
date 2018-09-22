4d61a5057a9ccd24ff1ef5bc6459d56f3eb73c75
Lucas Briese <lbriese@mail.uni-paderborn.de>
2018-09-22 21:05:00 +0200

    ~ created minimal test

--- Dockerfile
+++ Dockerfile
@@ -6,39 +6,23 @@ ENV STEAM_PATH="/home/steam" \
 	SERVER_PATH="/home/steam/server" \
 	GROUP_ID=10000 \
 	USER_ID=10000 \
-	DOCKER_USER=steam \
-	\
-	WORKSHOP_COLLECTION_ID= \
-	INSTALL_CSS=false \
-	INSTALL_HL2=false \
-	INSTALL_HLDM=false \
-	INSTALL_TF2=false \
-	\
-	CSS_PATH="/home/steam/addons/css" \
-	HL2_PATH="/home/steam/addons/hl2" \
-	HLDM_PATH="/home/steam/addons/hldm" \
-	TF2_PATH="/home/steam/addons/tf2" \
-	\
-	USE_MY_REPLACER_CONFIG=false
+	DOCKER_USER=steam
 	
 ENTRYPOINT ["./home/entrypoint.sh"]
 	
-COPY ["entrypoint.sh", "installAndMountAddons.sh", "forceWorkshopDownload.sh", "experimental.sh", "/home/"]
+COPY ["entrypoint.sh", "/home/"]
 
 # removed dep. lib32gcc1 libtcmalloc-minimal4:i386 gdb
 RUN dpkg --add-architecture i386 && \
 	apt-get update -y && \
-	apt-get install -y wget tar gzip ca-certificates lib32stdc++6 lib32tinfo5 \
-	lib32gcc1 libtcmalloc-minimal4:i386 gdb && \
+	apt-get install -y wget tar gzip ca-certificates lib32stdc++6 \
+	\
 	groupadd -g $GROUP_ID $DOCKER_USER && \
 	useradd -d /home/steam/ -g $GROUP_ID -u $USER_ID -m $DOCKER_USER && \
 	chown "$DOCKER_USER:$DOCKER_USER" /home/entrypoint.sh && \
 	mkdir -p "$SERVER_PATH" && \
 	chown -R "$DOCKER_USER:$DOCKER_USER" "$STEAM_PATH" && \
 	chmod a=rx /home/entrypoint.sh && \
-	chmod a=rx /home/installAndMountAddons.sh && \
-	chmod a=rx /home/forceWorkshopDownload.sh && \
-	chmod a=rx /home/experimental.sh && \
 	ulimit -n 2048 && \
 	\
 	locale-gen en_US.UTF-8
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,64 +3,21 @@
 echo "starting entrypoint.sh"
 set -e
 
-DEBUG_MODE=false
-if [ "$1" = "testing" ]; then
-	DEBUG_MODE=true
-fi
-
 echo "installing / updating steamcmd in $STEAM_PATH"
 cd "$STEAM_PATH"
 wget -q -O - "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -zxvf -
 
-if [ -e "${STEAM_PATH}/server/steam_cache" ]; then
-	echo "clearing steam cache"
-	rm -rf "${STEAM_PATH}/server/steam_cache/"
-fi
-if [ -e "${STEAM_PATH}/server/garrysmod/cache" ]; then
-	echo "clearing cache"
-	rm -rf "${STEAM_PATH}/server/garrysmod/cache/*"
-fi
-
 echo "testing steamcmd"
-chmod ug=rwx,o= steamcmd.sh
 ./steamcmd.sh -noasync +login anonymous +quit
 
 echo "installing / validating ttt"
 cd "$STEAM_PATH"
-./steamcmd.sh -noasync +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit || \
-	(echo '[error][1] catched => printing stderr.txt:' && \
-		cat "Steam/logs/stderr.txt" && \
-		echo '[error][1] <= printed' && \
-		./steamcmd.sh -noasync +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit) || \
-	(echo '[error][2] catched => printing stderr.txt:' && \
-		cat "Steam/logs/stderr.txt" && \
-		echo '[error][2] <= printed' && \
-		./steamcmd.sh -noasync +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit)
-
-echo "processing scripts before start"
-
-echo "experimental.sh"
-bash /home/experimental.sh
-#echo "forceWorkshopDownload.sh"
-#bash /home/forceWorkshopDownload.sh
-#echo "installAndMountAddons.sh"
-#bash /home/installAndMountAddons.sh
-
-
-if [ -e "$SERVER_PATH/custom.sh" ]; then
-	echo "existing: $SERVER_PATH/custom.sh"
-	bash "$SERVER_PATH/custom.sh"
-else
-	echo "not existing: $SERVER_PATH/custom.sh"
-fi
+./steamcmd.sh -noasync +login anonymous +force_install_dir "$STEAM_PATH/server/" +app_update 4020 validate +quit
 
 # todo catch => send killserver / quit
 cd "$STEAM_PATH/server/"
 trap 'pkill -15 srcds_run' SIGTERM
 
-if [ "$DEBUG_MODE" != "true" ]; then
-	./srcds_run -console -game garrysmod +gamemode terrortown "$@" &
-	wait "$!"
-else
-	echo "debug ended"
-fi
+#-console -game garrysmod +gamemode terrortown
+./srcds_run "$@" &
+wait "$!"
