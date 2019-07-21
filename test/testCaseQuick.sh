dca294b01ee726d7ad4d96b617b278d29650ecaa
Lucas Briese <lbriese@mail.uni-paderborn.de>
2019-07-21 23:20:03 +0200

    - cleanup tests

--- test/testCaseQuick.sh
+++ test/testCaseQuick.sh
@@ -2,6 +2,8 @@
 
 if [ "${DEBUGGING}" = "true" ]; then
 	set -o xtrace
+else
+	DEBUGGING="false"
 fi
 
 set -o errexit
@@ -10,11 +12,20 @@ set -o pipefail
 
 #bash test/testStyle.sh
 
-echo "[testBuild][INFO]build docker-ttt:ubuntu"
-docker rmi "jusito/docker-ttt:ubuntu" || true
-docker build -t "jusito/docker-ttt:ubuntu" "."
-echo "[testRun][INFO]running docker-ttt:ubuntu"
-if ! docker run -ti --name "JusitoTesting" --rm -e TEST_MODE=true -e DEBUGGING="${DEBUGGING}" "jusito/docker-ttt:ubuntu"; then
+echo "[testBuild][INFO]build"
+
+docker rmi "jusito/docker-ttt:lgsm_debian" || true
+docker build -t "jusito/docker-ttt:lgsm_debian" "./lgsm/"
+
+docker rmi "jusito/docker-ttt:gmod_debian" || true
+docker build -t "jusito/docker-ttt:gmod_debian" "./gmod/"
+
+docker rmi "jusito/docker-ttt:gmod_ttt_debian" || true
+docker build -t "jusito/docker-ttt:gmod_ttt_debian" "./TTT/"
+
+
+echo "[testRun][INFO]running"
+if ! docker run -ti --name "JusitoTesting" --rm -e TEST_MODE=true -e DEBUGGING="$DEBUGGING" "jusito/docker-ttt:gmod_ttt_debian"; then
 	echo "[testRun][ERROR]run test failed for docker-ttt:ubuntu"
 	exit 1
 fi
