1a91cc8e24a0ceaa562092a00449ce5086203b73
Lucas Briese <lbriese@mail.uni-paderborn.de>
2019-10-17 00:35:49 +0200

    - removed option which was only for myself

--- gmod/initConfig.sh
+++ gmod/initConfig.sh
@@ -54,12 +54,3 @@ fi
 if [ -n "${SERVER_VOICE_ENABLE}" ]; then
 	configReplace "sv_voiceenable" "$SERVER_VOICE_ENABLE"
 fi
-
-
-
-#this is a simple option for myself, but you can use it too
-if [ "$USE_MY_REPLACER_CONFIG" = "true" ] && [ ! -e "${SERVER_PATH}/garrysmod/data/jusito_ttt_entity_replace" ]; then
-	mkdir -p "${SERVER_PATH}/garrysmod/data/jusito_ttt_entity_replace"
-	wget -O "${SERVER_PATH}/garrysmod/data/jusito_ttt_entity_replace/config.txt" "https://raw.githubusercontent.com/jusito/ttt_entity_replace/master/config.txt.example_fas2"
-fi
-
