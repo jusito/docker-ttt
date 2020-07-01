6fe6859a8eb09f615af26e41d26cf5790515f114
Lucas Briese <lbriese@mail.uni-paderborn.de>
2020-07-01 21:48:27 +0200

    = fix, tests used cache / wrong tag printed

--- test/testBuild.sh
+++ test/testBuild.sh
@@ -13,10 +13,10 @@ set -o pipefail
 echo "[testBuild][INFO]build"
 
 docker rmi "jusito/docker-ttt:lgsm_debian" || true
-docker build -t "jusito/docker-ttt:lgsm_debian" "lgsm/"
+docker build --no-cache -t "jusito/docker-ttt:lgsm_debian" "lgsm/"
 
 docker rmi "jusito/docker-ttt:gmod_debian" || true
-docker build -t "jusito/docker-ttt:gmod_debian" "gmod/"
+docker build --no-cache -t "jusito/docker-ttt:gmod_debian" "gmod/"
 
 docker rmi "jusito/docker-ttt:gmod_ttt_debian" || true
-docker build -t "jusito/docker-ttt:gmod_ttt_debian" "TTT/"
+docker build --no-cache -t "jusito/docker-ttt:gmod_ttt_debian" "TTT/"
--- test/testCaseQuick.sh
+++ test/testCaseQuick.sh
@@ -17,7 +17,7 @@ bash test/testBuild.sh
 
 echo "[testRun][INFO]running"
 if ! docker run -ti --name "JusitoTesting" --rm -e TEST_MODE=true -e DEBUGGING="$DEBUGGING" -e SERVER_PASSWORD="testpw" -e SERVER_MAX_PLAYERS="10" "jusito/docker-ttt:gmod_ttt_debian"; then
-	echo "[testRun][ERROR]run test failed for docker-ttt:ubuntu"
+	echo "[testRun][ERROR]run test failed for docker-ttt:gmod_ttt_debian"
 	exit 1
 fi
 docker stop "JusitoTesting" || true
